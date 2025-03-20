7d0b2c8a6187dbf775a88fcf9426b622ddba9d37
PREMKUMARDUDI <premkumardudi312@gmail.com>
2025-03-20 22:39:11 +0530

    Update HoldingsContext to match original with refresh

--- dashboard/src/components/AuthContext.js
+++ dashboard/src/components/AuthContext.js
@@ -5,6 +5,8 @@ import axios from "axios";
 export const AuthContext = createContext();
 const BACKEND_URL =
   process.env.REACT_APP_BACKEND_URL || "http://localhost:3002";
+const FRONTEND_URL =
+  process.env.REACT_APP_FRONTEND_URL || "http://localhost:3000";
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
