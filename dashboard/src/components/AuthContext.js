89303d0885b1b798c1271c4f5ab90be576591f21
PREMKUMARDUDI <premkumardudi312@gmail.com>
2025-03-20 22:43:20 +0530

    Update HoldingsContext to match original with refresh

--- dashboard/src/components/AuthContext.js
+++ dashboard/src/components/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 
