77b9414d1fd2700bd8a092a469435822d6c2fb84
PREMKUMARDUDI <premkumardudi312@gmail.com>
2026-01-24 14:11:04 +0530

    modified newBuyOrder & newSellOrder controllers

--- backend/index.js
+++ backend/index.js
@@ -131,7 +131,7 @@ app.post("/newBuyOrder", async (req, res) => {
 
     let holding = await HoldingsModel.findOne({
       name: req.body.name,
-      price: req.body.price,
+      avg: req.body.avg,
     });
 
     if (holding) {
